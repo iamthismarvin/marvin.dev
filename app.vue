099d28d39195059339a5bb3586049e2c01534ab2
Marvin Parada <hello@marvin.dev>
2023-04-06 15:22:07 -0700

    Install ESLint & Prettier.

--- app.vue
+++ app.vue
@@ -3,12 +3,12 @@
 </template>
 
 <script setup lang="ts">
-const route = useRoute();
-const title = ref(`Marvin Parada | ${route.meta.title ?? "Software Engineer"}`);
+const route = useRoute()
+const title = ref(`Marvin Parada | ${route.meta.title ?? 'Software Engineer'}`)
 const description = ref(
-  "I'm Marvin. I build software. Make a positive social impact and shape a brighter future through technology. Want to work together? Let's talk!"
-);
-const image = ref("https://marvin-dev-test.netlify.app/open_graph.png");
+  "I'm Marvin. I build software. Make a positive social impact and shape a brighter future through technology. Want to work together? Let's talk!",
+)
+const image = ref('https://marvin-dev-test.netlify.app/open_graph.png')
 
 useSeoMeta({
   title,
@@ -19,5 +19,5 @@ useSeoMeta({
   twitterDescription: description,
   ogImage: image,
   twitterImage: image,
-});
+})
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
-  css: ["@/assets/css/main.css"],
-});
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
+  css: ['@/assets/css/main.css'],
+})
