ad65df8300299e060b921fc2eefc45511a8159c5
Marvin Parada <hello@marvin.dev>
2023-04-05 19:14:18 -0700

    Install Tailwind CSS.

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <div>Marvin Parada | Software Engineer</div>
+  <div class="flex items-center m-4">
+    <img src="@/assets/images/logo.svg" alt="Marvin Parada" class="h-8 mr-4" />
+  </div>
 </template>
