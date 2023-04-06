c4424e0a285a88bb786b3c08336a0dfd75e85c4f
Marvin Parada <hello@marvin.dev>
2023-04-05 18:33:43 -0700

    Initialize project.

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const title = ref(`Marvin Parada | ${route.meta.title ?? "Software Engineer"}`);
+const description = ref(
+  "I'm Marvin. I build software. Make a positive social impact and shape a brighter future through technology. Want to work together? Let's talk!"
+);
+const image = ref("https://marvin-dev-test.netlify.app/open_graph.png");
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  twitterTitle: title,
+  description,
+  ogDescription: description,
+  twitterDescription: description,
+  ogImage: image,
+  twitterImage: image,
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Marvin Parada | Software Engineer</div>
+</template>
